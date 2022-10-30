a654df0e06f30be910e62ab5a84e3b7e98408d7f
Megan <dommelmeg@gmail.com>
2022-10-30 18:30:08 -0400

    commit tree 2ee2a25b49218e2c63fce90853c9c5db246c56c9

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+const superbowlWin = record => {
+  const season = record.find(season => season.result === "W")
+  if(season) {
+    return season.year
+  } else {
+    return undefined
+  }
+}
+
+//Use find() to test each object to see if results is "W" & return year when win occurred
+
+//NEED TO LOOP THROUGH ARRAY
+//NEED IF ELSE STATEMENT
+//USE find() TO FIND A 'W'
+//NEED TO RETURN A YEAR OR UNDEFINED
